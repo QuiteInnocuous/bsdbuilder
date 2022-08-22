69a7b5fbf5e5c51c1b356ef169457d4e10bb6175
J Raymond Davidson <111265147+QuiteInnocuous@users.noreply.github.com>
2022-08-21 22:14:44 -0700

    Parse all files.
    There is a lot of cross-linking, so this may be necessary.

--- bsdbuilder/src/main/kotlin/com/quiteinnocuous/bsdbuilder/Main.kt
+++ bsdbuilder/src/main/kotlin/com/quiteinnocuous/bsdbuilder/Main.kt
@@ -1,6 +1,11 @@
 package com.quiteinnocuous.bsdbuilder
 
 import com.squareup.kotlinpoet.FunSpec
+import java.io.FileInputStream
+import java.io.InputStream
+import java.net.URI
+import java.net.URISyntaxException
+import java.nio.file.*
 import javax.xml.parsers.DocumentBuilderFactory
 
 fun main(args: Array<String>) {
@@ -8,9 +13,14 @@ fun main(args: Array<String>) {
     val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
 
     // Arbitrary classloader
-    val dom = with(FunSpec::class.java.classLoader.getResourceAsStream(args[0])) {
-        documentBuilder.parse(this)
+    Files.list(Paths.get(args[0])).filter {
+        it.toString().endsWith(".cat")
+    }.map {
+        with(
+            FileInputStream(it.toString()),
+            documentBuilder::parse
+        )
+    }.forEach {
+        println(it.childNodes.length)
     }
-
-    println(dom.getElementsByTagName("entryLink").length)
 }
