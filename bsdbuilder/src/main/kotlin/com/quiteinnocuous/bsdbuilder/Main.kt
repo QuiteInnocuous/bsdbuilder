c4872e1f0dd50f4d37decdfecfa99b06355e7943
J Raymond Davidson <111265147+QuiteInnocuous@users.noreply.github.com>
2022-08-23 22:20:18 -0700

    Map all doms by id

--- bsdbuilder/src/main/kotlin/com/quiteinnocuous/bsdbuilder/Main.kt
+++ bsdbuilder/src/main/kotlin/com/quiteinnocuous/bsdbuilder/Main.kt
@@ -4,14 +4,14 @@ import java.io.FileInputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 import javax.xml.parsers.DocumentBuilderFactory
+import kotlin.streams.asSequence
 
 fun main(args: Array<String>) {
 
     val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-    
-    Files.list(Paths.get(args[0])).filter {
-        it.fileName.toString().let {
-            fileName ->
+
+    val idsToDoms = Files.list(Paths.get(args[0])).filter {
+        it.fileName.toString().let { fileName ->
             !fileName.startsWith("ZZ") && fileName.endsWith(".cat")
         }
     }.map {
@@ -19,7 +19,12 @@ fun main(args: Array<String>) {
             FileInputStream(it.toString()),
             documentBuilder::parse
         )
-    }.forEach {
-        println(it.getElementsByTagName("catalogue").item(0).attributes.getNamedItem("name").nodeValue)
+    }.asSequence().associateBy {
+        it.getElementsByTagName("catalogue").item(0).attributes.getNamedItem("id").nodeValue
+    }
+
+    idsToDoms.forEach { (id, dom) ->
+        val name = dom.getElementsByTagName("catalogue").item(0).attributes.getNamedItem("name").nodeValue
+        println("$id => $name")
     }
 }
