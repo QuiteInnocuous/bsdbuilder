a4e342ac71143586b1d1fee96b6c3cfaf65470fd
J Raymond Davidson <111265147+QuiteInnocuous@users.noreply.github.com>
2022-09-07 22:28:54 -0700

    Added fields to entrylink

--- bsdbuilder/src/main/kotlin/com/quiteinnocuous/bsdbuilder/data/EntryLink.kt
+++ bsdbuilder/src/main/kotlin/com/quiteinnocuous/bsdbuilder/data/EntryLink.kt
@@ -3,5 +3,8 @@ package com.quiteinnocuous.bsdbuilder.data
 data class EntryLink(
     var id: String,
     var name: String,
-    var targetId: String
+    var targetId: String,
+    var hidden: Boolean,
+    var collective: Boolean,
+    var type: String
 )
